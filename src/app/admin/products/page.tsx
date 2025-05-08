2bdb634b39a4a82176315feca145db836642793d
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2025-05-08 12:28:45 +0200

    Fixes to be able to deploy

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -45,7 +45,7 @@
 
 
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '../../lib/authOptions';
 import { redirect } from 'next/navigation';
 
 import { getProducts } from '../../lib/contentful';
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,5 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: { label: "Användarnamn", type: "text" },
-        password: { label: "Lösenord", type: "password" }
-      },
-      async authorize(credentials) {
-        if (
-          credentials?.username === process.env.ADMIN_USERNAME &&
-          credentials?.password === process.env.ADMIN_PASSWORD
-        ) {
-          return { id: 'admin', name: 'Admin' };
-        }
-        return null;
-      }
-    })
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-  pages: {
-    signIn: '/login',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from 'next-auth';
+import { authOptions } from '../../../lib/authOptions';
 
 const handler = NextAuth(authOptions);
 
